324db224543fb43805c6dc718de16a7034ec064b
sh222td <sh222td@student.lnu.se>
2015-11-16 15:58:19 +0100

    Hämtar ur samtliga filmer/tider de kan kolla

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ ini_set('display_errors', 'Off');
 
 checkButton();
 
+$availableMoviesToWatch = array();
 $url = $_POST['url'];
 $data = curl_get_request($url);
 $dom = new DomDocument();
@@ -34,13 +35,37 @@ if ($dom->loadHTML($data)) {
     $indexArray = array();
 
     $calendarIndex =  $items->item(0)->getAttribute("href");
-    $calenderData = getCalendarItems(trimSlashFromUrl($url).$calendarIndex, $dom);
+    $availableDays = getCalendarItems(trimSlashFromUrl($url).$calendarIndex, $dom);
 
     $movieIndex = $items->item(1)->getAttribute("href");
-    $movieData = getMovieItems(trimSlashFromUrl($url).$movieIndex, $dom);
+    $getMovies = getMovieValues(trimSlashFromUrl($url).$movieIndex, $dom);
+    $movieOptions = checkAvailableCinema($availableDays, $getMovies, trimSlashFromUrl($url).$movieIndex);
 
+    foreach ($movieOptions as $movieOption) {
+        if ($movieOption["movie"] == 01) {
+            $movieOption["movie"] = "Söderkåkar";
+        }
+        if ($movieOption["movie"] == 02) {
+            $movieOption["movie"] = "Fabian Bom";
+        }
+        if ($movieOption["movie"] == 03) {
+            $movieOption["movie"] = "Pensionat Paradiset";
+        }
+        if ($movieOption[0] == 01) {
+            $movieOption[0] = "Fredag";
+        }
+        if ($movieOption[0] == 02) {
+            $movieOption[0] = "Lördag";
+        }
+        if ($movieOption[0] == 03) {
+            $movieOption[0] = "Söndag";
+        }
+        array_push($availableMoviesToWatch, $movieOption);
+    }
 
-    //print_r($calenderData);
+    foreach ($availableMoviesToWatch as $movie) {
+        echo "Alla kan se ".$movie["movie"]." klockan ".$movie["time"]." på ".$movie[0]."<br/>";
+    }
 }
 
 // Tar bort alla "/" från slutet av huvud url:en
@@ -69,12 +94,13 @@ function curl_get_request($url) {
 
     return $data;
 }
-
+// Hämtar ut url:erna för varje person och gör ett anrop till funktionen getPersonCalendar
+// Returnerar funktionen checkAvailableCinemaDays som har dagarna i en array
 function getCalendarItems($calendar, $dom) {
     $personArray = array();
     $data = curl_get_request($calendar);
-    libxml_use_internal_errors(true);
 
+    libxml_use_internal_errors(true);
     if ($dom->loadHTML($data)) {
         libxml_use_internal_errors(false);
         $xpath = new DOMXPath($dom);
@@ -91,25 +117,26 @@ function getCalendarItems($calendar, $dom) {
         $maryIndex =  $items->item(2)->getAttribute("href");
         $maryArray = getPersonCalendar($calendar."/".$maryIndex, $dom);
         array_push($personArray, $maryArray);
-
-        checkAvailableCinemaDays($personArray);
     }
+    return checkAvailableCinemaDays($personArray);
 }
 
+//Lägger in tillgänliga dagar i en array
 function checkAvailableCinemaDays($personArray) {
     $availableDays = array();
     if ($personArray[0][0] == $personArray[1][0] && $personArray[0][0] == $personArray[2][0] && $personArray[1][0] == $personArray[2][0]) {
-        array_push($availableDays, "Fredag");
+        array_push($availableDays, "01");
     }
     if ($personArray[0][1] == $personArray[1][1] && $personArray[0][1] == $personArray[2][1] && $personArray[1][1] == $personArray[2][1]) {
-        array_push($availableDays, "Lördag");
+        array_push($availableDays, "02");
     }
     if ($personArray[0][2] == $personArray[1][2] && $personArray[0][2] == $personArray[2][2] && $personArray[1][2] == $personArray[2][2]) {
-        array_push($availableDays, "Söndag");
+        array_push($availableDays, "03");
     }
     return $availableDays;
 }
 
+// Funktion som kollar vilka dagar varje person har sagt "OK" på
 function getPersonCalendar($person, $dom) {
     $personCalendarArray = array();
     $data = curl_get_request($person);
@@ -122,29 +149,46 @@ function getPersonCalendar($person, $dom) {
         foreach ($items as $item) {
             array_push($personCalendarArray, strtolower($item->nodeValue));
         }
-
         return $personCalendarArray;
     }
 }
 
-function getMovieItems($movie, $dom) {
-
+// Funktion som hämtar ur filmernas värde, lägger sedan in dom i arrayen $moviearray
+function getMovieValues($movieUrl, $dom) {
     $movieArray = array();
-    $data = curl_get_request($movie);
+    $data = curl_get_request($movieUrl);
     libxml_use_internal_errors(true);
 
     if ($dom->loadHTML($data)) {
         libxml_use_internal_errors(false);
         $xpath = new DOMXPath($dom);
-        $items = $xpath->query('//h1');
-
+        $items = $xpath->query('//form/select[@id="movie"]/option');
         foreach ($items as $item) {
-            $collections = $item->nodeValue;
-            $movieArray[] = array($collections);
+            $movieValue = $item->getAttribute("value");
+            if ($movieValue != null) {
+                array_push($movieArray, $movieValue);
+            }
         }
-        echo "<pre>";
-        print_r($movieArray);
-        echo "</pre>";
         return $movieArray;
     }
+}
+
+// Funktion som gör en get på samtliga filmer på dagarna som de satt som tillgängliga samt filmernas tider
+// decodar json objektet som istället omvandlas till en array
+function checkAvailableCinema($availableDays, $movies , $movieUrl) {
+    $availableMoviesDayAndTimeArray = array();
+
+    foreach ($availableDays as $days) {
+        foreach ($movies as $movie) {
+            $data = curl_get_request($movieUrl."/check?day=".$days."&movie=".$movie);
+            $options = json_decode($data, true);
+            foreach ($options as $option) {
+                if ($option["status"] != 0) {
+                    array_push($option, $days);
+                    array_push($availableMoviesDayAndTimeArray, $option);
+                }
+            }
+        }
+    }
+    return $availableMoviesDayAndTimeArray;
 }
